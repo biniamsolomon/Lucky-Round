e561c73496e62e013a2e3851d63c6d483be6e05e
biniam.solomon <biniam.solomon@safaricom.et>
2025-05-06 10:52:48 +0300

    first commit

---
+++ src/main/java/com/bini/Lucky_Round/LuckyRoundApplication.java
@@ -0,0 +1,13 @@
+package com.bini.Lucky_Round;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyRoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckyRoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bini/Lucky_Round/LuckyRoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bini.Lucky_Round;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyRoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
